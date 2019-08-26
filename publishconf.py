47b38aadb536b05f9fa079323639179ae4825d4a
Farrel Franqois <farrel@franqois.id>
2019-08-26 17:19:48 +0700

    Merubah Kode Branch

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://farrel.franqois.id'
-CDN_THEME_REPO_BRANCH = '6d9371b'
+CDN_THEME_REPO_BRANCH = '87c9ad7'
 CDN_STATIC_THEME_URL = f'https://cdn.statically.io/gh/FarrelF/Modified-Flex/{CDN_THEME_REPO_BRANCH}/static'
 RELATIVE_URLS = False
 
