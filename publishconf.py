27dd0b0a1b74d424ccc5747a09d3e6c1b7505b1a
Farrel Franqois <farrel@franqois.id>
2019-08-27 22:55:49 +0700

    Menambahkan dan Konfigurasi 'Response Header'

--- publishconf.py
+++ publishconf.py
@@ -12,13 +12,23 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://farrel.franqois.id'
+
+# Penggunaan CDN
 USE_CDN = True
 CDN_THEME_REPO_BRANCH = '87c9ad7'
 CDN_STATIC_THEME_URL = f'https://cdn.statically.io/gh/FarrelF/Modified-Flex/{CDN_THEME_REPO_BRANCH}/static'
-RELATIVE_URLS = False
 CC_LICENSE['distribution-type'] = 'cdn'
+
+# URL Relatif
+RELATIVE_URLS = False
+
+# Feed/RSS
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+# Menambahkan Header saat di terbitkan nanti
+STATIC_PATHS.append('extras/_headers')
+EXTRA_PATH_METADATA['extras/_headers'] = {'path': '_headers'}
 
 # Activating Cache
 CACHE_PATH = 'cache'
